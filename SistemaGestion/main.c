f5286e9550ac14267b58b5ef9d8c748fb7c9cc34
Afaiik <50887871+Afaiik@users.noreply.github.com>
2019-06-23 05:53:14 +0000

    Menu eliminar usuarios y gestion de usuarios
    Se continua con menu y login (listo)
    Se hace funcion de modificar password

--- SistemaGestion/main.c
+++ SistemaGestion/main.c
@@ -40,8 +40,11 @@ typedef struct Factura
     float PrecioTotal;
 }Factura;
 
-///Carga el archivo de Usuarios
+///Crea usuarios en loop
 void cargaArchivoUsuarios();
+
+///Crear un usuario
+void crearUnUsuario();
 
 ///Guarda un usuario (Usada por cargaArchivoUsuarios)
 void guardarUsuario(Usuario usua);
@@ -72,33 +75,40 @@ Usuario getUsuarioById(int idBuscado, char arUsuarios[]);
 Usuario getUsuarioByNombre(char nombreBuscado[], char arUsuarios[]);
 
 ///Baja Logica de usuario buscandolo por ID (Activo = 0)
-int eliminarUsuarioById(int usuaId, char arUsuarios[]);
+int eliminarUsuarioById(int usuaId);
 
-
-
+///TITULOS
 void showTituloLogin();
 void showTituloBienvenido();
 
+///Funcion auxiliar
+void enDesarrollo();
 
+///MENU LOGIN
 void menuLogin();
 void doLogin();
+
+///MENU PRINCIPAL Toma como parametro el usuario que se logueo para saber que opciones mostrar
+void menuPrincipal(Usuario usuaLogueado);
+
+///MENU GESTION USUARIOS
+void menuGestionUsuarios(Usuario usuaLogueado);
+
+///Modificar Password de Usuario
+void modificarPassword(Usuario usuaLogueado, char newPass[30]);
+void menuModificarPassword(Usuario usuaLogueado);
+
+///Eliminar usuarios
+void interfazEliminarUsuarioById(Usuario usuaLogueado);
+void menuEliminarUsuarios(Usuario usuaLogueado);
+void eliminarMiUsuario(Usuario usuaLogueado);
 
 
 int main()
 {
-    Usuario efra[50];
-    Usuario usuaAux;
+    Usuario usuarioLogueado;
 
-    int val = 0;
-    int dim = 50;
-    int testBusqueda;
-    int testAdmin;
-    char nombreAux[30];
-    char nombreTest[30];
-    //cargaArchivoUsuarios();
-
-    //mostrarArchivoUsuarios(archivoUsuarios);
-
+    mostrarArchivoUsuarios(archivoUsuarios);
     menuLogin();
 
     return 0;
@@ -154,7 +164,6 @@ void doLogin()
             getch();
             gotoxy(0,0);
             system("cls");
-
         }else
         {
             system("cls");
@@ -164,7 +173,7 @@ void doLogin()
             getch();
             gotoxy(0,0);
             system("cls");
-            menuPrincipal();
+            menuPrincipal(usua);
         }
     }
 }
@@ -172,78 +181,259 @@ void doLogin()
 void menuLogin()
 {
     int opcion;
-    system("cls");
+    int opcionValida = 0;
+    ///system("cls");
     showTituloBienvenido();
     printf("\n");
-    printf("\n1- Crear Usuario");
-    printf("\n2- Loguearse");
-    printf("\n3- *inserte una opcion loca por aqui(?)*\n ");
+    printf("\n\t1- Crear Usuario");
+    printf("\n\t2- Loguearse\n\t");
 
     opcion = getch();
     printf("\n %d", opcion);
+    while(opcionValida != 1)
+    {
+        switch(opcion) {
 
-    switch(opcion) {
+            case 49:
+                opcionValida = 1;
+                crearUnUsuario();
+                break;
 
-       case 49:
-          crearUnUsuario();
-          break;
+            case 50:
+                opcionValida = 1;
+                doLogin();
+                break;
 
-       case 50:
-          doLogin();
-          break;
-
-       case 51:
-          enDesarrollo();
-          break;
-
-       default :
-       printf("fucku");
+            default:
+                system("cls");
+                gotoxy(30,10);
+                color(4);
+                printf("OPCION INVALIDA");
+                getch();
+                gotoxy(0,0);
+                system("cls");
+        }
     }
-
-
 }
 
-void menuPrincipal()
+void menuPrincipal(Usuario usuaLogueado)
 {
     int opcion;
+    int opcionValida = 0;
     system("cls");
     showTituloBienvenido();
-    printf("\n");
-    printf("\n1- Gestionar productos");
-    printf("\n2- Gestionar usuarios");
-    printf("\n3- Facturar\n ");
-
+    
+    if(usuaLogueado.Tipo == TIPOADMIN){
+        printf("\n");
+        printf("\n\t1- Gestionar productos");
+        printf("\n\t2- Gestionar usuarios");
+        printf("\n\t3- Facturar\n ");
+    }else{
+        printf("\n");
+        printf("\n\t1- Facturar");
+        printf("\n\t2- Listar productos");
+        printf("\n\t3- Cambiar password\n ");
+    }
     opcion = getch();
     printf("\n %d", opcion);
+    while(opcionValida != 1)
+    {
+        switch(opcion) {
 
-    switch(opcion) {
+           case 49:
+               opcionValida = 1;
+               if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    enDesarrollo(usuaLogueado);///gestionar productos
+               }else
+               {
+                   enDesarrollo(usuaLogueado);///Facturar
+               }
+              break;
 
-       case 49:
-          enDesarrollo();
-          break;
+           case 50:
+               opcionValida = 1;
+                if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    menuGestionUsuarios(usuaLogueado);
+               }else
+               {
+                   enDesarrollo(usuaLogueado);///listar productos
+               }
+              break;
 
-       case 50:
-          enDesarrollo();
-          break;
+           case 51:
+               opcionValida = 1;
+                if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    enDesarrollo(usuaLogueado);///facturar
+               }else
+               {
+                   menuModificarPassword(usuaLogueado);
+               }
+              break;
 
-       case 51:
-          enDesarrollo();
-          break;
-
-       default :
-       printf("fucku");
+           default :
+            printf("OPCION INVALIDA");
+        }
     }
 
+
 }
-void enDesarrollo()
+void enDesarrollo(Usuario usuaLogueado)
 {
     system("cls");
     printf("SECCION EN DESARROLLO");
     getch();
     system("cls");
-    menuLogin();
+    menuPrincipal(usuaLogueado);
 }
 
+void menuGestionUsuarios(Usuario usuaLogueado)
+{
+    int opcion;
+    int opcionValida = 0;
+    system("cls");
+    showTituloBienvenido();
+
+    if(usuaLogueado.Tipo == TIPOADMIN){
+        printf("\n");
+        printf("\n\t1- Modificar mi password");
+        printf("\n\t2- Mostrar usuarios");
+        printf("\n\t3- Mostrar administradores");
+        printf("\n\t4- Eliminar usuarios");
+    }else{
+        printf("\n");
+        printf("\n\t1- Modificar mi password");
+        printf("\n\t2- Eliminar mi usuario");
+    }
+
+    while(opcionValida != 1)
+    {
+        opcion = getch();
+
+        switch(opcion) {
+
+           case 49:
+                opcionValida = 1;
+                menuModificarPassword(usuaLogueado);
+              break;
+
+           case 50:
+               opcionValida = 1;
+              if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    enDesarrollo(usuaLogueado);///mostrar usuarios
+               }else
+               {
+                   enDesarrollo(usuaLogueado);///eliminar mi usuario
+               }
+              break;
+
+           case 51:
+              if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    opcionValida = 1;
+                    enDesarrollo(usuaLogueado);
+               }
+              break;
+           case 52:
+              if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    opcionValida = 1;
+                    menuEliminarUsuarios(usuaLogueado);
+               }
+              break;
+
+           default :
+            printf("OPCION INVALIDA");
+        }
+    }
+}
+
+void modificarPassword(Usuario usuaLogueado, char newPass[30])
+{
+    Usuario usuaAux;
+    FILE *pArch= fopen(archivoUsuarios, "r+b");
+    if(pArch != NULL)
+    {
+        while(fread(&usuaAux, sizeof(Usuario), 1, pArch) > 0)
+        {
+            if(usuaAux.Id == usuaLogueado.Id)
+            {
+                strcpy(usuaLogueado.Contra, newPass);
+                fseek(pArch,sizeof(Usuario) * (-1), SEEK_CUR);
+                fwrite(&usuaLogueado,sizeof(Usuario), 1, pArch);
+            }
+        }
+    fclose(pArch);
+    }
+
+}
+
+void menuModificarPassword(Usuario usuaLogueado)
+{
+    char newPass[30];
+    char newPassAux[30];
+    char passActual[30];
+    int valido = 0;
+    
+    while(valido != 1)
+        {
+            
+        while(valido != 1)
+        {
+            system("cls");
+            printf("\n\t\t<<<<<<<<<<Modificacion de Password>>>>>>>>>>>");
+            printf("\nIngrese su password actual......:   ");
+            fflush(stdin);
+            scanf("%s", passActual);
+            if(strcmp(strlwr(passActual),strlwr(usuaLogueado.Contra)) != 0)
+            {
+                system("cls");
+                printf("\a");
+                gotoxy(15,5);
+                printf("Ese no es su password !!");
+                getch();
+                gotoxy(0,0);
+                system("cls");
+            }else
+            {
+                valido = 1;
+            }
+        }
+        valido = 0;
+        printf("\nIngrese su nuevo password........:   ");
+        fflush(stdin);
+        scanf("%s", newPass);
+        printf("Repita su nuevo password...........:   ");
+        scanf("%s", newPassAux);
+        if(strcmp(strlwr(newPass), strlwr(newPassAux)) == 0)
+        {
+            valido = 1;
+
+            modificarPassword(usuaLogueado, newPass);
+
+            system("cls");
+            printf("\a");
+            gotoxy(15,5);
+            printf("SU CONTRASEÑA HA SIDO CAMBIADA ! :)");
+            getch();
+            gotoxy(0,0);
+            system("cls");
+        }else
+        {
+            system("cls");
+            printf("\a");
+            gotoxy(15,5);
+            printf("DATOS INCORRECTOS !");
+            getch();
+            gotoxy(0,0);
+            system("cls");
+        }
+    }
+    menuLogin();
+}
 
 void cargaArchivoUsuarios(){
 
@@ -298,6 +488,7 @@ void crearUnUsuario()
     int codAdmin;
     int valido = 0;
     char nombreAux[30];
+    int checkExist = 2;
     usua.Activo = 1;
     usua.Id = getUltimoIdUsuario(archivoUsuarios)+1;
     system("cls");
@@ -309,7 +500,10 @@ void crearUnUsuario()
         printf("\nNombre de Usuario..............:");
         fflush(stdin);
         scanf("%s", &nombreAux);
-        if(checkExisteUsuarioNombre(nombreAux,archivoUsuarios) == 0)
+        checkExist = checkExisteUsuarioNombre(nombreAux, archivoUsuarios);
+        printf("CHECKEXIST ----->>>>> %d", checkExist);
+        system("pause");
+        if((checkExisteUsuarioNombre(nombreAux, archivoUsuarios) == 0) || (checkExisteUsuarioNombre(nombreAux, archivoUsuarios) == -1))
         {
             valido = 1;
             strcpy(usua.Nombre, nombreAux);
@@ -336,11 +530,13 @@ void crearUnUsuario()
         usua.Tipo = TIPONORMAL;
 
     guardarUsuario(usua);
+    menuLogin();
 }
-
+       
 void guardarUsuario(Usuario usua){
-    FILE *pArchUsuarios = fopen(archivoUsuarios,"ab");
+    FILE *pArchUsuarios = fopen(archivoUsuarios,"a+b");
     if(pArchUsuarios != NULL){
+        fseek(pArchUsuarios, sizeof(Usuario), SEEK_END);
         fwrite(&usua, sizeof(Usuario),1,pArchUsuarios);
         fclose(pArchUsuarios);
     }
@@ -458,6 +654,7 @@ int checkExisteUsuarioNombre(char nombreBuscado[], char arUsuarios[])
         }
         fclose(pArch);
     }
+    
    return flag;
 }
 
@@ -570,30 +767,216 @@ Usuario getUsuarioByNombre(char nombreBuscado[], char arUsuarios[])
    return usua;
 }
 
-int eliminarUsuarioById(int usuaId, char arUsuarios[])
+void interfazEliminarUsuarioById(Usuario usuaLogueado)
+{
+    int usuaId = -1;
+    int valido = 0;
+    Usuario usuaAux;
+    char opcion;
+    int flag = 0;
+
+    while(valido == 0)
+    {
+        system("cls");
+        printf("\nIntroduzca el ID del usuario a ELIMINAR");
+        scanf("%d", &usuaId);
+        if(getUltimoIdUsuario(archivoUsuarios)<= usuaId)
+        {
+            if(checkExisteUsuarioId(usuaId, archivoUsuarios) == 0)
+            {
+                valido = 1;
+                usuaAux = getUsuarioById(usuaId, archivoUsuarios);
+                printf("\nEsta seguro que desea eliminar al siguiente usuario ?");
+                mostrarUnUsuario(usuaAux);
+                printf("\nS/N");
+                fflush(stdin);
+                scanf("%c", opcion);
+                if((opcion == 'S') || (opcion == 's'))
+                {
+                    flag = eliminarUsuarioById(usuaId);
+                    if(flag == 1)
+                    {
+                        system("cls");
+                        printf("\a");
+                        gotoxy(15,5);
+                        printf("\nEliminacion exitosa.");
+                        getch();
+                        gotoxy(0,0);
+                        system("cls");
+                        menuGestionUsuarios(usuaLogueado);
+                    }else
+                    {
+                        
+                        system("cls");
+                        printf("\a");
+                        gotoxy(15,5);
+                        printf("\nNo se pudo completar la operacion.");
+                        getch();
+                        gotoxy(0,0);
+                        system("cls");
+                        menuGestionUsuarios(usuaLogueado);
+                    }
+                }
+
+            }else
+            {
+                if(checkExisteUsuarioId(usuaId, archivoUsuarios) == -1)
+                {
+                    printf("MENOS UNOOOO");
+                    getch();
+                }
+                system("cls");
+                printf("\a");
+                gotoxy(15,5);
+                printf("\nEl usuario no existe !!");
+                getch();
+                gotoxy(0,0);
+                system("cls");
+            }
+        }
+    }
+}
+
+
+int eliminarUsuarioById(int usuaId)
 {
     Usuario usua;
-    int flag = -1; /// -1 error no abrio archivo, 0 no es admin, 1 es admin
+    int flag = 0;
     FILE *pArch;
-    pArch = fopen(arUsuarios, "rb");
+    pArch = fopen(archivoUsuarios, "r+b");
 
     if(pArch != NULL)
     {
-        while(fread(&usua, sizeof(Usuario), 1, pArch)>0)
+        while((flag == 0) && (fread(&usua, sizeof(Usuario), 1, pArch)>0))
         {
             if(usua.Id == usuaId)
             {
-                if(usua.Tipo == 1)
+                if(usua.Activo == 1)
+                {
                     flag = 1;
+                    usua.Activo = 0;
+                    fseek(pArch, sizeof(Usuario)*(-1), SEEK_CUR);
+                    fwrite(&usua, sizeof(Usuario), 1, pArch);
+                }
+
             }
-        }
-        if(flag != 1)
-        {
-            flag = 0;
         }
         fclose(pArch);
     }
    return flag;
 }
 
+void eliminarMiUsuario(Usuario usuaLogueado)
+{
+    Usuario usua;
+    int flag = 0;
+    FILE *pArch;
+    char opcion;
+    printf("cls");
+    printf("\nESTA SEGURO DE ELIMINAR SU USUARIO ??");
+    fflush(stdin);
+    scanf("%c", opcion);
+    if((opcion == 'S') || (opcion == 's'))
+    {
+        flag = eliminarUsuarioById(usuaLogueado.Id);
+        if(flag == 1)
+        {
+            system("cls");
+            printf("\a");
+            gotoxy(15,5);
+            printf("\nEliminacion exitosa.");
+            getch();
+            gotoxy(0,0);
+            system("cls");
+            menuLogin();
+        }else
+        {
+            
+            system("cls");
+            printf("\a");
+            gotoxy(15,5);
+            printf("\nNo se pudo completar la operacion.");
+            getch();
+            gotoxy(0,0);
+            system("cls");
+            menuLogin();
+        }
+    }
+}
 
+
+
+void menuEliminarUsuarios(Usuario usuaLogueado)
+{
+        
+    int opcion;
+    int opcionValida = 0;
+    system("cls");
+
+    if(usuaLogueado.Tipo == TIPOADMIN )
+    {
+        printf("\n\t\t<<<<<<<<<ELIMINACION DE USUARIOS>>>>>>>>>");
+        printf("\n1-Eliminar usuario por Id");
+        printf("\n2-Eliminar usuario por Nombre");
+        printf("\n3-Eliminar mi usuario");
+        printf("\n4-Listar usuarios por nombre");
+        printf("\n5-Listar usuarios por ID");
+    }else
+    {
+        printf("\n\t\t<<<<<<<<<ELIMINACION DE USUARIOS>>>>>>>>>");
+        printf("\n1-Eliminar mi usuario");
+    }
+    
+    while(opcionValida != 1)
+    {
+        opcion = getch();
+
+        switch(opcion) {
+
+            case 49:
+                opcionValida = 1;
+                if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    interfazEliminarUsuarioById(usuaLogueado);
+               }else
+               {
+                   eliminarMiUsuario(usuaLogueado);
+               }
+              break;
+
+            case 50:
+               
+              if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    opcionValida = 1;
+                    enDesarrollo(usuaLogueado);///ELIMINAR USUARIO POR NOMBRE
+               }
+              break;
+
+            case 51:
+              if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    opcionValida = 1;
+                    eliminarMiUsuario(usuaLogueado);
+               }
+              break;
+            case 52:
+              if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    opcionValida = 1;
+                    menuEliminarUsuarios(usuaLogueado);///LISTAR USUARIOS POR NOMBRE
+               }
+              break;
+            case 53:
+              if(usuaLogueado.Tipo == TIPOADMIN)
+               {
+                    opcionValida = 1;
+                    menuEliminarUsuarios(usuaLogueado);///LISTAR USUARIOS POR ID
+               }
+              break;
+
+           default :
+            printf("OPCION INVALIDA");
+        }
+    }
+}
